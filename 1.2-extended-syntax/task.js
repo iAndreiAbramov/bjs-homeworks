d9d4e8b9e7fe4a461355f1cb072ebfccaf8796b6
Andrei <i.andrei.abramov@gmail.com>
2021-02-25 21:18:24 +0300

    Correction_25.02

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -2,7 +2,11 @@
 
 function getResult(a, b, c) {
 
-    if (typeof (+a) != 'number' || typeof (+b) != 'number' || typeof (+c) != 'number') {
+    a = +a;
+    b = +b;
+    c = +c;
+
+    if (typeof(a) != 'number' || typeof(b) != 'number' || typeof(c) != 'number') {
         console.error('Некорректно заданы аргументы');
         return;
     }
@@ -13,8 +17,8 @@ function getResult(a, b, c) {
 
     if (D === 0) result.push((-b / 2 * a));
     if (D > 0) {
-        result.push(((-1*b + Math.pow(D, 0.5)) / (2 * a)));
-        result.push(((-1*b - Math.pow(D, 0.5)) / (2 * a)));
+        result.push(((-b + Math.sqrt(D)) / (2 * a)));
+        result.push(((-b - Math.sqrt(D)) / (2 * a)));
     }
 
     return result;
@@ -26,11 +30,9 @@ function getAverageMark(marks) {
 
     if (num === 0) return 0;
 
-    if (num > 5) {
-        return marks.slice(0, 5).reduce((sum, cur) => sum += cur) / 5;
-    } else {
-        return marks.reduce((sum, cur) => sum += cur) / num;
-    }
+    const totalCount = marks.slice(0, 5).reduce((sum, cur) => sum += cur);
+
+    return (num < 5) ? totalCount / num : totalCount / 5;
 
 }
 
@@ -39,27 +41,14 @@ function askDrink(name, dateOfBirthday) {
     let currentDate = new Date();
 
     const currentYear = currentDate.getFullYear();
-    // console.log(currentYear);
-    const currentMonth = currentDate.getMonth();
-    // console.log(currentMonth);
-    const currentDay = currentDate.getDate();
-    // console.log(currentDay);
 
     const birthYear = dateOfBirthday.getFullYear();
-    // console.log(dateOfBirthday, birthYear);
-    const birthMonth = dateOfBirthday.getMonth();
-    // console.log(dateOfBirthday, birthMonth);
-    const birthDay = dateOfBirthday.getDate();
-    // console.log(dateOfBirthday, birthDay);
 
-    const ageMonths = (currentYear - birthYear) * 12 + (currentMonth - birthMonth);
-    // console.log(ageMonths);
-
-    if (ageMonths > 216  || (ageMonths === 216 && currentDay >= birthDay)) {
-        alert(`Не желаете ли олд-фэшн, ${name}?`)
-    } else if (ageMonths > 0 || (ageMonths === 0 && currentDay >= birthDay)) {
-        alert(`Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`)
+    const majorityAge = 18;
+ 
+    if (currentYear >= birthYear + majorityAge) {
+        return(`Не желаете ли олд-фэшн, ${name}?`)
     } else {
-        alert(`Простите, ${name}, но Вы еще не родились. Давайте не будем торопить события.`)
+        return(`Сожалею, ${name}, но я не могу вам продать алкоголь. Могу предложить вам замечательный клюквенный компот!`)
     }
 }
