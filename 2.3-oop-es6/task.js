472f753287eaa1ca336747891f4e2140193453b7
Andrei <i.andrei.abramov@gmail.com>
2021-03-12 16:34:09 +0300

    correction_12.03.2021

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -51,24 +51,21 @@ class Book extends PrintEditionItem {
 
 class NovelBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.author = author;
+    super(author, name, releaseDate, pagesCount);
     this.type = 'novel';
   }
 }
 
 class FantasticBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.author = author;
+    super(author, name, releaseDate, pagesCount);
     this.type = 'fantastic';
   }
 }
 
 class DetectiveBook extends Book {
   constructor(author, name, releaseDate, pagesCount) {
-    super(name, releaseDate, pagesCount);
-    this.author = author;
+    super(author, name, releaseDate, pagesCount);
     this.type = 'detective';
   }
 }
@@ -100,6 +97,20 @@ class Library {
   }
 
 }
+
+// const library = new Library("Библиотека имени Ленина");
+
+// library.addBook(new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008));
+// library.addBook(new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168));
+// library.addBook(new NovelBook("Герберт Уэллс", "Машина времени", 1895, 138));
+// library.addBook(new Magazine("Мурзилка", 1924, 60));
+
+// console.log(library.findBookBy("name", "Властелин колец")); //null
+// console.log(library.findBookBy("releaseDate", 1924).name); //"Мурзилка"
+
+// console.log("Количество книг до выдачи: " + library.books.length); //Количество книг до выдачи: 4
+// library.giveBookByName("Машина времени");
+// console.log("Количество книг после выдачи: " + library.books.length); //Количество книг после выдачи: 3
 
 // Задача №3
 class StudentLog {
